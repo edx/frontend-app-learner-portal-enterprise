12a02a3ae1d84dc4718350c9b2f3b231d0800d3c
sjasti-sonata-svg <sjasti-sonata@2u.com>
2026-04-29 05:08:02 +0000

    feat(ENT-11384): render New Content facet at end of filter row

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -13,6 +13,7 @@ import { NUM_RESULTS_PER_PAGE } from './constants';
 import SearchProgram from './SearchProgram';
 import SearchCourse from './SearchCourse';
 import { ContentHighlights } from './content-highlights';
+import CustomSearchFilters from './CustomSearchFilters';
 import { features } from '../../config';
 
 import { IntegrationWarningModal } from '../integration-warning-modal';
@@ -181,6 +182,7 @@ const Search = () => {
               index={searchIndex}
               filters={filters}
               enterpriseConfig={enterpriseCustomer}
+              filterComponents={<CustomSearchFilters />}
             />
           </div>
         )}
